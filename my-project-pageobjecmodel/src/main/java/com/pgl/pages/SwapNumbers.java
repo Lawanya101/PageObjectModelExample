01b1e6d3365b4842f2c8f254d3c5b1a50949f8a7
Lawanya101 <lawanyapalnisamy@gmail.com>
2026-01-17 20:18:49 +0530

    Added print statements in Swap number program

--- my-project-pageobjecmodel/src/main/java/com/pgl/pages/SwapNumbers.java
+++ my-project-pageobjecmodel/src/main/java/com/pgl/pages/SwapNumbers.java
@@ -6,19 +6,19 @@ public class SwapNumbers {
 	int num2=6;
 
 	public void swapNumbers1(){
-		System.out.println(num1 +" "+num2);
+		System.out.println("num 1 is " + num1 +" num 2 is "+num2);
 		num1=num1+num2;
 		num2=num1-num2;
 		num1=num1-num2;
-		System.out.println(num1 +" "+num2);	
+		System.out.println("num 1 is " + num1 +" num 2 is "+num2);	
 	}
 		
 	public void swapNumbers2(){
-		System.out.println(num1 +" "+num2);
+		System.out.println("num 1 is " + num1 +" num 2 is "+num2);
 		num1=num1*num2;
 		num2=num1/num2;
 		num1=num1/num2;
-		System.out.println(num1 +" "+num2);
+		System.out.println("num 1 is " + num1 +" num 2 is "+num2);
 	}
 	
 	public static void main(String[] args) {
